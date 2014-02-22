3558abdbec72590714191091d48a572505d3666e
eanderson13 <erik.anderson13@hotmail.com>
2014-02-22 16:35:47 -0700

    Implemented variable size

--- Column.java
+++ Column.java
@@ -1,20 +1,22 @@
-// The "Column" class.
+// The "Column1" class.
 import hsa.*;
-public class Column
+public class Column1
 {
     public static void main (String[] args)
     {
-	int n1, n2, n3, n4, n5;
+	int n1, n2, n3, n4, n5, width;
+	Stdout.print ("Enter the column width: ");
+	width = Stdin.readInt ();
 	Stdout.print ("Enter 5 numbers: ");
 	n1 = Stdin.readInt ();
 	n2 = Stdin.readInt ();
 	n3 = Stdin.readInt ();
 	n4 = Stdin.readInt ();
 	n5 = Stdin.readInt ();
-	Stdout.println (n1, 5);
-	Stdout.println (n2, 5);
-	Stdout.println (n3, 5);
-	Stdout.println (n4, 5);
-	Stdout.println (n5, 5);
+	Stdout.println (n1, width);
+	Stdout.println (n2, width);
+	Stdout.println (n3, width);
+	Stdout.println (n4, width);
+	Stdout.println (n5, width);
     } // main method
-} // Column class
+} // Column1 class
